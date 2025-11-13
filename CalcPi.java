4be08c07203a27b971cb604c8a51c97264a153c5
NoaSz <noa.szafran@post.runi.ac.il>
2025-11-13 22:14:48 +0200

    hjk

--- CalcPi.java
+++ CalcPi.java
@@ -3,14 +3,13 @@ public class CalcPi {
         int n = Integer.parseInt(args[0]);
         double piApprox = 0.0;
 
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             piApprox += Math.pow(-1.0, i) / (2.0 * i + 1.0);
         }
 
         piApprox *= 4.0;
 
         System.out.println("pi according to Java: 3.141592653589793");
-       
-        System.out.printf("pi, approximated: %20.15f%n", piApprox);
+        System.out.printf("pi, approximated: %.15f%n", piApprox);
     }
 }
